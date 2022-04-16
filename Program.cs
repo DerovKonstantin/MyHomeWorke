87f50b52959ede739b849b2b8584381278b1a617
DiorOFF <DiorOFF!inbox.ru>
2022-04-16 22:25:50 +0700

    Add Zadacha2

--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
 ﻿/*
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-
-Console.Write("Input integer number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-int result = num * num;
-
-Console.WriteLine("Result is " + result);
-
-
-
 // Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 
-// Первый вариант оформления
+// Первый вариант оформления первой задачи
 
 int a = -9;
 int b = -3;
@@ -31,7 +17,7 @@ if (a < b) max = b;
 Console.Write("max = ");
 Console.WriteLine(max);
 
-// Второй вариант оформления
+// Второй вариант оформления первой задачи
 
 Console.Write("Input first integer number: ");
 int a = Convert.ToInt32(Console.ReadLine());
@@ -44,6 +30,32 @@ if (a > b)
     max = a;
 else
     max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+
+// Решение второй задачи
+
+Console.Write("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third integer number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max =num1;
+
+if (num1 > max)  max = num1;
+if (num2 > max)  max = num2;
+if (num3 > max)  max = num3;
 
 Console.Write("max = ");
 Console.WriteLine(max);
