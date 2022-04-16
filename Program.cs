3088a8c5ed460d24374e3b0f94e2e71c95ce6b2a
DiorOFF <DiorOFF!inbox.ru>
2022-04-16 15:36:20 +0700

    add TEST

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+Console.Write("Input integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num * num;
+
+Console.WriteLine("Result is " + result);
+
+*/
