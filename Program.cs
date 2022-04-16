4ab93180e05df47f94c4600ab99e08cb3f84276e
DiorOFF <DiorOFF!inbox.ru>
2022-04-16 22:10:36 +0700

    Add Zadacha1

--- Program.cs
+++ Program.cs
@@ -20,27 +20,32 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 
-
-
-Console.Write("Input first integer number: ");
-int a - Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second integer number: ");
-int b - Convert.ToInt32(Console.ReadLine());
-
-if (a > b) max = a;
-if (a < b) max = b;
-
-Console.WriteLine ("max = " + max)
-
-*/
+// Первый вариант оформления
 
 int a = -9;
 int b = -3;
-
-int max = a;
 
 if (a > b) max = a;
 if (a < b) max = b;
 
 Console.Write("max = ");
 Console.WriteLine(max);
+
+// Второй вариант оформления
+
+Console.Write("Input first integer number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max =a;
+
+if (a > b) 
+    max = a;
+else
+    max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+*/
