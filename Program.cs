63a977d90f1d9dbbfc9ec07e30c3fceb4b865359
DiorOFF <DiorOFF!inbox.ru>
2022-04-20 15:25:41 +0700

    add gitignore seminar2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Пример задачи из семинара
+void ReduceNumber()
+{
+    int num = new Random().Next(100,999);
+    Console.Write(num + " -> ");
+    int num1 = num / 100;
+    int num2 = num % 10;
+
+    Console.Write(num1);
+    Console.Write(num2);
+}
+
+ReduceNumber();
