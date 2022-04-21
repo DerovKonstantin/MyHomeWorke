b8611a2539eadef1e98c8b6f26147ec9e58bf0c6
DiorOFF <DiorOFF!inbox.ru>
2022-04-21 12:06:57 +0700

    Add zadacha13

--- Program.cs
+++ Program.cs
@@ -22,7 +22,6 @@ ReduceNumber();
 782 -> 8
 918 -> 1
 
-*/
 // Решение задачи №10
 
 void SecondDigit()
@@ -39,3 +38,42 @@ void SecondDigit()
 
 SecondDigit();
 
+
+
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+*/
+
+void SecondDigit(int num)
+
+{
+    if( num < 100)
+    {
+        Console.WriteLine( " No third digit " );
+    }
+    else
+    {
+         while (num > 1000)
+    {
+        num = num / 10;
+        
+    } 
+    if (num < 1000)
+        {
+            num = num % 10;
+        }
+    Console.Write(num);
+    Console.WriteLine( " " );
+    }
+}
+
+Console.Write("Input three digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(num + " -> ");
+SecondDigit(num);
