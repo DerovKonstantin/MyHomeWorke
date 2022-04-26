346f98aeb23aaa9b9f8ce4829f115e1392d3ea1d
DiorOFF <DiorOFF!inbox.ru>
2022-04-26 16:35:32 +0700

    Zadacha 34; 36

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿// Пример из семинра (генератор рандомных чисел)
+﻿
+/*
+// Пример из семинра (генератор рандомных чисел)
 
 int[] CrreateRandomArray(int size, int min, int max)
 
@@ -48,12 +50,106 @@ FindeSumOfElements(myArray);
 
 
 
-/*
+
 
 // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
 
+// Решение задачи 34
+
+int[] CrreateRandomArray(int size, int min, int max)
+
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+void FindeEvenNumbers (int[] array)
+{
+    int ENum = 0;
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            // sum = sum + i;
+            // sum += i;
+            if(array[i] %2 == 0)
+                ENum = ENum + 1;
+            else
+                ENum = ENum + 0;
+        }
+        else
+                ENum = ENum + 0;
+        
+    }
+    Console.WriteLine("The number of even numbers in the array " + ENum);
+}
+
+int[] myArray = CrreateRandomArray(3,0,10);
+ShowArray(myArray);
+FindeEvenNumbers (myArray);
+
 */
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+// Решение задачи 36
+
+int[] CrreateRandomArray(int size, int min, int max)
+
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+void SumElementsOddPositions (int[] array)
+{
+    int SEONum = 0;
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 == 0)
+            SEONum = SEONum + array[i];
+        else
+            SEONum = SEONum + 0;
+    }
+    Console.WriteLine("Sum of elements of odd positions is " + SEONum);
+}
+
+int[] myArray = CrreateRandomArray(20,0,10);
+ShowArray(myArray);
+SumElementsOddPositions(myArray);
 
 
