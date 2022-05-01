58fd83d74102360f8c8247ab2a11e015cb8aa6c8
DiorOFF <DiorOFF!inbox.ru>
2022-05-01 22:03:36 +0700

    Add zadacha 3 seminar 5

--- Program.cs
+++ Program.cs
@@ -67,11 +67,31 @@ Console.Write("Input number: ");
 int expnum = Convert.ToInt32(Console.ReadLine());
 Exponentiation (expnum);
 
-*/
+---------------------------------------------------------------------------------------------
 
 //  Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 //  1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 //  6, 1, 33 -> [6, 1, 33]
+
+// Решение
+*/
+
+Console.Clear();
+void ShowArrayFor(int[] arrayfor)
+{
+    int length = arrayfor.Length;
+    int index = 0;
+    while (index < length)
+    {
+        arrayfor[index] = new Random().Next(1,100);
+        Console.Write(arrayfor[index] + ", ");
+        index++;
+    }
+   
+    Console.WriteLine(" ");
+}
+
+ShowArrayFor(new int[8]);
 
